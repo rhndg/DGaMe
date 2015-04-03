34fbe6ff88430f84f3c86d9a685f28baa1784bae
rohan_das <rohandas36@gmail.com>
2015-04-03 22:36:47 +0530

    distributed in files

--- src/bullet.h
+++
@@ -1,9 +0,0 @@
-#ifndef BULLET_H
-#define BULLET_H
-
-#include <bits/stdc++.h>
-#include "Vector3.h"
-
-using namespace std;
-
-#endif
--- src/player.h
+++
@@ -1,9 +0,0 @@
-#ifndef PLAYER_H
-#define PLAYER_H
-
-#include <bits/stdc++.h>
-#include "Vector3.h"
-
-using namespace std;
-
-#endif
