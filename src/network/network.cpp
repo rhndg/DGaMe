b347e5189be13613d343220c0bdfb7eb96fe78ad
Abhishek Gupta <abhi19gupta@gmail.com>
2015-04-20 21:20:25 +0530

    network

--- src/network/network.cpp
+++ src/network/network.cpp
@@ -6,6 +6,81 @@ Network::Network(){
 Network::Network(string host){
 
 }
-vector< vector<game_map::key_tap> > Network::Synchronize(vector<game_map::key_tap> moves){
-	
+void Network::clearTemp(){
+	syncData.clear();
+	gameData.clear();
+	syncReqs.clear();
+	gameReqs.clear();
+	syncBuf.clear();
+	gameBuf.clear();
+	packId=0;
 }
+vector<char> Network::bufInit(packType t,int packId,int packCount){
+	vector<char> buffer;
+	int x;
+	if (t!=controlPack)
+		x=dataSession/256;
+	else
+		x=controlSession/256;
+	buffer.push_back(((char)x);
+	if (t!=controlPack)
+		x=dataSession%256;
+	else
+		x=controlSession%256;
+	buffer.push_back(((char)x);
+	buffer.push_back(((char)playerId));
+	buffer.push_back(((char)t));
+	x=packId/256;
+	buffer.push_back(x);
+	x=packId%256;
+	buffer.push_back(x);
+	x=packCount/256;
+	buffer.push_back(x);
+	x=packCount%256;
+	buffer.push_back(x);
+	return buffer;
+}
+
+void Network::encodeSync(vector<game_map::key_tap> moves){
+	vector<char> buffer=bufInit(syncPack,0,1);
+	while(moves.size()<mvsLen) moves.push_back(nop);
+	buffer.push_back(((char)playerId));
+	for(int i = 0;i<mvsLen;i+=2){
+		int x=(int)moves.back();
+		moves.pop_back();
+		x=x*16+((int)moves.back());
+		buffer.push_back(((char)x));
+		moves.pop_back();
+	}
+	buffer.resize(packSize,((char)-1));
+	for(int i=1;i<=numPlayers;i++){
+		if (isPeer[i]) syncBuf.push_back(pair<int,vector<char> >(i,buffer));
+	}
+}
+
+void Network::encodeSyncReqs(){
+	vector<char> temp=bufInit(syncPack,0,1),buffer;
+	for(int j=1;j<=numPlayers;j++){
+		if(isPeer[j]){
+			reqIds=syncReqs[j];
+			for(int i = 0;i!=reqIds.size();i++){
+				buffer=temp;
+				vector<game_map::key_tap> moves = syncData[reqIds[i]];
+				if(moves.size()!=0){
+					moves.resize(mvsLen,nop);
+					buffer.push_back(((char)reqIds[i]));
+					while(!moves.empty()){
+						int x=(int)moves.back();
+						moves.pop_back();
+						x=x*16+((int)moves.back());
+						buffer.push_back(((char)x));
+						moves.pop_back();
+					}
+				}
+			}
+		buffer.resize(packSize,((char)-1));
+		syncBuf.push_back(pair<int,vector<char> >(j,buffer));
+		}
+	}
+}
+
--- src/network/network.h
+++ src/network/network.h
@@ -5,6 +5,7 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <errno.h>
+#include <netdb.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -17,23 +18,56 @@ using namespace std;
 
 class Network {
 private:
-	int movNum,NumPlayers,Session,PlayerId,packSize,packCount;	//number of moves(keytaps)session id (never 0),number of players, id of this player
-	map< int,vector < pair<int,sockaddr_in> > > address;		//socket addressed of given id
-	map< int,bool > IsPeer;										//current connected peers
-	map< int,vector <int> > Request;							//requested data of players by the given id
-	vector< vector<game_map::key_tap> data;						//recieved data
-	vector<char> buffer;										//buffer
-	float delay;												//time delay for message to reach
-	void analyze();												//analyzes recieved packets and updates state
-	void construct(int Id);										//constructs packets to be sent as per requests 
-	void sendbuf(int Id);										//send contents of the buffer to the specified id
-	void wait();												//wait for time delay
-	void clearTemp();											//clears temp data for restarting process
+	
+	enum packType {
+		syncPack,gamePack,controlPack,syncReqPack,gameReqPack
+	};
+//control and data session should be distinct
+	int movNum,numPlayers,dataSession,controlSession,playerId,packSize,packCount,mvsLen	//number of moves(keytaps)
+		,maxTime,minTime,sendGameTo,sockNum;												//session id (never 0)
+																		//number of players
+																		//id of this player
+																		//size of packet
+																		//number of redundant packets
+																		//length allotted to moves in synchro data
+																		//
+	game_map* dGame
+
+	map< int,vector <sockaddr_in> > address;					//socket addressed of given id
+	map< int,bool > isPeer;										//current connected peers
+	map< int,vector<game_map::key_tap> > syncData;				//recieved data
+	map< pair<int,int>,vector<char> > gameData;					//recieved data for recon to be sent data for rec data
+	map< int,vector <int> > syncReqs;						//requested data of players by the given id
+	map< int,vector <int,int> > gameReqs;					//requested data of players by the given id
+
+	vector<pair<int,vector<char> > > syncBuf;					//contains sync data
+	vector<vector<char> > gameBuf;
+	int delay;													//time delay for message to reach
+	
+
+	vector<char> bufInit(packType type,int packId,int packCount);	//push the first 8 bytes acc to pack type
+
+	void encodeSync(vector<game_map::key_tap> moves);
+	void encodeSyncReqs();
+	void encodeGameReqs();
+	void encodeGame(int forId,game_map* aGame);						//encodes game state		
+	void decodeSync(vector<char> pack);								//analyzes recieved packets and updates state
+	void decodeGame(vector<char> pack,game_map* aGame);				//updates agame with data
+	void decodeSyncReq(vector<char> pack);
+	void decodeGameReq();
+
+	void sendSyncBuf();												//send contents of the buffer
+	void sendGameBuf();
+	void recBuf();													//loads all data from socket to buffer wih int field -1
+	void wait();													//wait for time delay
+	void incCounter();												//increaments counters
+	void clearTemp();												//clears temp data for restarting process
 public:
 	Network();
 	Network(string host);
 	~Network();
-	vector< vector<game_map::key_tap> > Synchronize(vector<game_map::key_tap> v);
+	void* data_thread(void* x);
+	void* control_thread(void* x);
 	//vector of non human players, data recieved ***empty if disconnected
 };
 #endif
